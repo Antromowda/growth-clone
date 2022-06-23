1b66fd92905f9c8b0aba1c0a6524b8483c34dd7f
Enrico B. Palma <enricob.palma@gmail.com>
2022-06-23 15:54:35 -0300

    feat: added fonts

--- fe/src/assets/styles/global.js
+++ fe/src/assets/styles/global.js
@@ -5,5 +5,10 @@ export default createGlobalStyle`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    font-family: 'Inter', sans-serif;
+  }
+
+  body {
+    font-size: 16px;
   }
 `;
